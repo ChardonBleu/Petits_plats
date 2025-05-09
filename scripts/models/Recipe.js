b445f62cbf1d2e718f466d12dcbb30edc9989803
ChardonBleu <f.rondi@orange.fr>
2025-05-09 16:33:39 +0200

    ♻️ refactor: refacto and add descriptions

--- scripts/models/Recipe.js
+++ scripts/models/Recipe.js
@@ -4,10 +4,17 @@ export class Recipe {
     this.image = data.image; //string
     this.name = data.name; //string
     this.servings = data.servings; //number
-    this.ingredients = data.ingredients; // array
+    this.ingredients = data.ingredients; // array of objects {ingredient, quantity, unity}
     this.time = data.time; //number
     this.description = data.description; //string
     this.appliance = data.appliance; //string
-    this.ustensils = data.ustensils; // array
+    this.ustensils = data.ustensils; // array of strings
+  }
+
+  get ingredientsList() {
+    // Array if strings
+    return this.ingredients.map((ingredient) =>
+      ingredient.ingredient.toLowerCase(),
+    );
   }
 }
--- scripts/pages/recipes.js
+++ scripts/pages/recipes.js
@@ -3,11 +3,15 @@ import { updateRecipeCounter } from "./counter.js";
 import {
   getTagsListsFromRecipes,
   updateListTagsFromRecipes,
-  filterRecipesWithTags
+  filterRecipesWithTags,
 } from "../search/tags.js";
 import { searchRecipes } from "../search/recipeSearch.js";
 import { sanitize } from "../utils/functions.js";
 
+/** display active recipes
+ * @param recipes {object}  app.recipes from current app instance
+ * @return undefined
+ */
 export function displayRecipes(recipes) {
   const section = document.getElementById("recipes");
   section.innerHTML = "";
@@ -18,36 +22,54 @@ export function displayRecipes(recipes) {
   updateRecipeCounter(recipes);
 }
 
+/** display page with new active datas: recipes and tag lists
+ * @param app {object} - app instance from App class
+ * @return undefined
+ */
 export function displayAndManageIndexPage(app) {
   getTagsListsFromRecipes(app);
   updateListTagsFromRecipes(app);
   displayRecipes(app.recipes);
 }
 
-export function eventOnPrincipalSearch(inputSearch, app) {
+/** manage input validity before search
+ * @param inputSearch {DOM node}  input node with input user for principal search
+ * @return Array(boolean, string)  boolean for validation and string for sanitized input
+ *  */
+export function validateSearchInput(inputSearch) {
   const searchError = document.getElementById("searchError");
   if (inputSearch.checkValidity()) {
     searchError.classList.add("hidden");
     searchError.classList.remove("flex");
-    const searchString = sanitize(inputSearch.value);
-    searchRecipes(app, searchString);
+    return [true, sanitize(inputSearch.value)];
   } else {
     searchError.classList.remove("hidden");
     searchError.classList.add("flex");
+    return [true, ""];
   }
 }
 
+/** eventListeners for principal search
+ * @param app {object} - app instance from App class
+ * @return undefined
+ */
 export function managePrincipalSearch(app) {
   const searchBtn = document.getElementById("principalSearchBtn");
   const inputSearch = document.getElementById("prinicpalSearchInput");
   const clearSearchBtn = document.getElementById("clearPrincipalSearchBtn");
   searchBtn.addEventListener("click", () => {
-    eventOnPrincipalSearch(inputSearch, app);
+    const [valid, searchString] = validateSearchInput(inputSearch);
+    if (valid) {
+      searchRecipes(app, searchString);
+    }
   });
   inputSearch.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
-      eventOnPrincipalSearch(inputSearch, app);
+      const [valid, searchString] = validateSearchInput(inputSearch);
+      if (valid) {
+        searchRecipes(app, searchString);
+      }
     }
   });
   clearSearchBtn.addEventListener("click", () => {
@@ -58,9 +80,16 @@ export function managePrincipalSearch(app) {
   });
 }
 
+/** When user makes tag search while principal search has non empty input
+ * @param app {object} - app instance from App class
+ * @return undefined
+ */
 export function controlForActivePrincipalSearch(app) {
   const inputSearch = document.getElementById("prinicpalSearchInput");
   if (inputSearch.value) {
-    eventOnPrincipalSearch(inputSearch, app);
+    const [valid, searchString] = validateSearchInput(inputSearch);
+    if (valid) {
+      searchRecipes(app, searchString);
+    }
   }
 }
