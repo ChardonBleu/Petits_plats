4fb17511260921e172e016a33f3d20d741f04dc3
ChardonBleu <f.rondi@orange.fr>
2025-05-15 16:08:24 +0200

    ♻️ refactor: refacto App.js

--- scripts/pages/recipes.js
+++ scripts/pages/recipes.js
@@ -1,12 +1,5 @@
 import { recipeCardTemplate } from "../components/recipeCard.js";
 import { updateRecipeCounter } from "./counter.js";
-import {
-  getTagsListsFromRecipes,
-  displayTagsCard,
-  manageTags,
-} from "../pages/tags.js";
-import { ingredients, appliances, ustensils } from "../utils/constants.js";
-import { manageTagsSearch } from "../search/tagsFilters.js";
 
 /** display active recipes
  * @param recipes {object}  app.recipes from current app instance
@@ -20,22 +13,4 @@ export function displayRecipes(recipes) {
     section.appendChild(recipeArticle);
   });
   updateRecipeCounter(recipes);
-}
-
-/** display page with new active datas: recipes and tag lists
- * @param app {object} - app instance from App class
- * @return undefined
- */
-export function displayAndManageIndexPage(app) {
-  getTagsListsFromRecipes(app);
-  displayTagsCard(app.ingredients, ingredients);
-  displayTagsCard(app.appliances, appliances);
-  displayTagsCard(app.ustensils, ustensils);
-  manageTags(ingredients.en);
-  manageTags(appliances.en);
-  manageTags(ustensils.en);
-  manageTagsSearch(app, ingredients.en);
-  manageTagsSearch(app, appliances.en);
-  manageTagsSearch(app, ustensils.en);
-  displayRecipes(app.recipes);
 }
